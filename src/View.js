c23dd907b3a30920532baca1ea8556e913060403
Eduard Jacobs <edote92@gmail.com>
2021-02-23 05:08:13 -0800

    eslint with prettier

---
+++ src/View.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const ViewPage = () => {
+  return (
+    <View>
+      <Text>Helloo</Text>
+    </View>
+  );
+};
+
+export default ViewPage;
